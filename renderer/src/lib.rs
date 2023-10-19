d35034a8625ad81961fd94d9c2a919a89f2d2a20
Hasegawa Raku <raku@avesia.org>
2023-10-19 20:25:07 +0900

    Add dependencies

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub fn create_renderer(id: &str) {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas_container = document.get_element_by_id(id).unwrap();
+    let canvas = document.create_element("canvas").unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    canvas_container.append_child(&canvas).unwrap();
+
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into().unwrap();
+
+    let width = canvas.width();
+    let height = canvas.height();
+
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        dx12_shader_compiler: Default::default(),
+    });
+
+    let surface = instance.create_surface_from_canvas(canvas);
 }
